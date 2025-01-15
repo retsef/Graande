dd83142d4b3a3d54c462b8f0fe730d19324e105a
Roberto Scinocca <roberto.scinocca@gmail.com>
2025-01-15 17:52:20 +0100

    update adapter config

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,11 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			strict: false
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
 		}),
 		paths: {
 			base: dev ? '' : process.env.BASE_PATH,
